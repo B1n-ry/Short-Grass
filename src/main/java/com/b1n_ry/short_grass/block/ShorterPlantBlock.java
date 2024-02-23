67b52c31b0a0ec38c0fe7572bb1cfbf86d77192f
B1n_ry <b1nry.dev@gmail.com>
2024-02-23 23:50:02 +0100

    Updated to 1.20.4

--- src/main/java/com/b1n_ry/short_grass/block/ShorterPlantBlock.java
+++ src/main/java/com/b1n_ry/short_grass/block/ShorterPlantBlock.java
@@ -2,16 +2,16 @@ package com.b1n_ry.short_grass.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.FernBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.block.ShortPlantBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
-public class ShortFernBlock extends FernBlock {
+public class ShorterPlantBlock extends ShortPlantBlock {
     protected static final VoxelShape SHAPE = Block.createCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 4.0D, 14.0D);
 
-    public ShortFernBlock(Settings settings) {
+    public ShorterPlantBlock(Settings settings) {
         super(settings);
     }
 
